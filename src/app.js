3a737f26d5dafff47a03629e43b7de4c395dfafb
shoni76 <46452369+shoni76@users.noreply.github.com>
2019-10-25 18:25:10 +0300

    module4

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const accounts = JSON.parse(accountData);
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
 const users = JSON.parse(userData);
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.urlencoded({extended: true}));
 app.get('/', (req, res) =>{
   res.render('index', {title : 'Account Summary', accounts});
 });
@@ -24,6 +24,24 @@ app.get('/checking', (req,res) =>{
 });
 app.get('/credit', (req,res) =>{
   res.render('account', {account: accounts.credit});
+});
+
+app.get('/transfer', (req, res) => res.render('transfer'));
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+  res.render('transfer', {message: 'Transfer Completed'});
+});
+
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+app.post('/payment', (req,res) =>{
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available += parseInt(req.body.amount, 10);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+  res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 });
 
 app.get('/profile', (req, res) =>{
